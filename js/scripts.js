396ad53644b456c0173828d29995d8a48b08f740
Brian Hensley <brnhensley@gmail.com>
2019-04-05 12:06:24 -0700

    cleaned up unused code

--- js/scripts.js
+++ js/scripts.js
@@ -22,21 +22,8 @@ function boopNumber(inputNumberString) {
   return arrayNumbers.join("");
 }
 
-  // if (i === 666) {
-    //   console.log("Hail Satan!")
-   // else if ((i % 3 === 0) && (i % 5 === 0)){
-//     console.log("I'm sorry, Dave. I'm afraid I can't do that")
-//   } else if (i % 3 === 0){
-//     console.log("Boop!")
-//   } else if (i % 5 === 0){
-//     console.log("Beep!")
-//   } else
-//   console.log(i);
-// }
-
   //  User Logic
-
-
+  
 $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
