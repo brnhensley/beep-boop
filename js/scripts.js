c4dbda67fa56b9d27576c0c502e97d54c36beb85
Brian Hensley <brnhensley@gmail.com>
2019-04-05 09:25:46 -0700

    prevent page from reloading on button submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,29 @@
   //  Business Logic
 
-for (var i = 1; i <= userInput; i++) {
-  if (i === 666) {
-    console.log("Hail Satan!")
-  } else if ((i % 3 === 0) && (i % 5 === 0)){
-    console.log("I'm sorry, Dave. I'm afraid I can't do that")
-  } else if (i % 3 === 0){
-    console.log("Boop!")
-  } else if (i % 5 === 0){
-    console.log("Beep!")
-  } else
-  console.log(i);
+function boopNumber(userInput) {
+  console.log(userInput);
 }
+// for (var i = 1; i <= userInput; i++) {
+//   if (i === 666) {
+//     console.log("Hail Satan!")
+//   } else if ((i % 3 === 0) && (i % 5 === 0)){
+//     console.log("I'm sorry, Dave. I'm afraid I can't do that")
+//   } else if (i % 3 === 0){
+//     console.log("Boop!")
+//   } else if (i % 5 === 0){
+//     console.log("Beep!")
+//   } else
+//   console.log(i);
+// }
 
   //  User Logic
 
-$(function() {
-  event.preventDefault():
-  var userInput = $("#inputNumber").val();
-  var output = boopIt(userInput);
-  $("#output").text(output);
-}
+
+  $(function() {
+    $("form").submit(function(event) {
+      event.preventDefault();
+      var userInput = $("#inputNumber").val();
+      var output = boopNumber(userInput);
+      $("#output").text(output);
+    })
+  })
