1f27b91099abe5588aef031a05e86c782cac7968
Brian Hensley <brnhensley@gmail.com>
2019-04-05 16:11:14 -0700

    WORK! It will work bacwards now
    q

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,10 @@ function boopNumber(inputNumberString) {
   var userNumber = parseInt(inputNumberString);
   var arrayNumbers = [];
   console.log(userNumber);
-  console.log(typeof userNumber)
-  console.log(typeof inputNumberString)
+  console.log(typeof userNumber);
+  console.log(typeof inputNumberString);
   if (userNumber === 666) {
-    $("#satan").show()
+    $("#satan").show();
   }
   for (var i = 0; i <= userNumber; i++) {
     if (i === 666) {
@@ -26,51 +26,50 @@ function boopNumber(inputNumberString) {
 }
 
       //  Reverse
-// function boopReverse(inputNumberString) {
-//   var userNumber = parseInt(inputNumberString);
-//   var arrayNumbers = [];
-//   console.log(userNumber);
-//   console.log(typeof userNumber)
-//   console.log(typeof inputNumberString)
-//   console.log(arrayNumbers);
-//   console.log(inputNumberString);
-//   if (userNumber === 666) {
-//     $("#satan").show()
-//   }
-//   for (var i = 0; i <= userNumber; i++) {
-//     if (i === 666) {
-//       arrayNumbers.shift(i + " Hail Satan! " + i + ", ");
-//     } else if (String(i).includes("3")){
-//       arrayNumbers.shift("I'm sorry, Dave. I'm afraid I can't do that. ");
-//     } else if (String(i).includes("2")){
-//       arrayNumbers.shift("Boop! ");
-//     } else if (String(i).includes("1")){
-//       arrayNumbers.shift("Beep! ");
-//     } else {
-//       arrayNumbers.shift(i + ", ");
-//     }
-//   }
-//   return arrayNumbers.join("");
-// }
+function boopReverse(inputNumberString) {
+  var userNumber = parseInt(inputNumberString);
+  var arrayNumbers = [];
+  console.log(userNumber);
+  console.log(typeof userNumber);
+  console.log(typeof inputNumberString);
+  if (userNumber === 666) {
+    $("#satan").show();
+  }
+  for (var i = 0; i <= userNumber; i++) {
+    if (i === 666) {
+      arrayNumbers.unshift(i + " !nataS liaH " + i + ", ");
+    } else if (String(i).includes("3")){
+      arrayNumbers.unshift(".taht od t'nac I diarfa m'I .evaD ,yrros m'I ");
+    } else if (String(i).includes("2")){
+      arrayNumbers.unshift("!pooB ");
+    } else if (String(i).includes("1")){
+      arrayNumbers.unshift("!peeB ");
+    } else {
+      arrayNumbers.unshift(i + ", ");
+    }
+  }
+  return arrayNumbers.join("");
+}
 
   //  User Logic
       //  Normal submit
 $(function() {
   $("form#beepboop").submit(function(event) {
     event.preventDefault();
-    $("#satan").hide()
+    $("#satan").hide();
     var inputNumberString = $("#inputNumber").val();
     var output = boopNumber(inputNumberString);
     $("#output").text(output);
   });
 });
       //  Reverse submit
-// $(function() {
-//   $("form#reverse").submit(function(event) {
-//     event.preventDefault();
-//     $("#satan").hide()
-//     var inputNumberString = $("#inputNumber").val();
-//     var output = boopReverse(inputNumberString);
-//     $("#output").text(output);
-//   });
-// });
+$(function() {
+  $("form#reverse").submit(function(event) {
+    event.preventDefault();
+    $("#satan").hide();
+    var inputNumberString = $("#inputReverseNumber").val();
+    console.log(inputNumberString);
+    var output = boopReverse(inputNumberString);
+    $("#output").text(output);
+  });
+});
