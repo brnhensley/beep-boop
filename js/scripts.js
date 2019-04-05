3b441d048d3023b48f3c3c0413633a77b633a4c3
Brian Hensley <brnhensley@gmail.com>
2019-04-05 15:57:14 -0700

    add new form for reverse answers and new function in js for it, it is not working yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
   //  Business Logic
-
+      //  Normal
 function boopNumber(inputNumberString) {
   var userNumber = parseInt(inputNumberString);
   var arrayNumbers = [];
@@ -8,10 +8,6 @@ function boopNumber(inputNumberString) {
   console.log(typeof inputNumberString)
   if (userNumber === 666) {
     $("#satan").show()
-    function play(){
-      var audio = document.getElementById("audio");
-      audio.play();
-      }
   }
   for (var i = 0; i <= userNumber; i++) {
     if (i === 666) {
@@ -29,10 +25,38 @@ function boopNumber(inputNumberString) {
   return arrayNumbers.join("");
 }
 
-  //  User Logic
+      //  Reverse
+// function boopReverse(inputNumberString) {
+//   var userNumber = parseInt(inputNumberString);
+//   var arrayNumbers = [];
+//   console.log(userNumber);
+//   console.log(typeof userNumber)
+//   console.log(typeof inputNumberString)
+//   console.log(arrayNumbers);
+//   console.log(inputNumberString);
+//   if (userNumber === 666) {
+//     $("#satan").show()
+//   }
+//   for (var i = 0; i <= userNumber; i++) {
+//     if (i === 666) {
+//       arrayNumbers.shift(i + " Hail Satan! " + i + ", ");
+//     } else if (String(i).includes("3")){
+//       arrayNumbers.shift("I'm sorry, Dave. I'm afraid I can't do that. ");
+//     } else if (String(i).includes("2")){
+//       arrayNumbers.shift("Boop! ");
+//     } else if (String(i).includes("1")){
+//       arrayNumbers.shift("Beep! ");
+//     } else {
+//       arrayNumbers.shift(i + ", ");
+//     }
+//   }
+//   return arrayNumbers.join("");
+// }
 
+  //  User Logic
+      //  Normal submit
 $(function() {
-  $("form").submit(function(event) {
+  $("form#beepboop").submit(function(event) {
     event.preventDefault();
     $("#satan").hide()
     var inputNumberString = $("#inputNumber").val();
@@ -40,3 +64,13 @@ $(function() {
     $("#output").text(output);
   });
 });
+      //  Reverse submit
+// $(function() {
+//   $("form#reverse").submit(function(event) {
+//     event.preventDefault();
+//     $("#satan").hide()
+//     var inputNumberString = $("#inputNumber").val();
+//     var output = boopReverse(inputNumberString);
+//     $("#output").text(output);
+//   });
+// });
