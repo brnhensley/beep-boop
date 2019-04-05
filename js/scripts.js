ea55920cb6f7f4e010194c6fef536c8e009503c1
Brian Hensley <brnhensley@gmail.com>
2019-04-05 10:08:28 -0700

    count to the user's input by printing it to the screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
   //  Business Logic
 
 function boopNumber(userInput) {
+  var userNumber = parseInt(userInput);
+  var arrayNumbers = []
+
   console.log(userInput);
+  for (var i = 0; i <= userInput; i++) {
+    arrayNumbers.push(i + ", ")
+  }
+
+  return arrayNumbers.join("");
 }
-// for (var i = 1; i <= userInput; i++) {
-//   if (i === 666) {
-//     console.log("Hail Satan!")
-//   } else if ((i % 3 === 0) && (i % 5 === 0)){
+
+  // if (i === 666) {
+    //   console.log("Hail Satan!")
+   // else if ((i % 3 === 0) && (i % 5 === 0)){
 //     console.log("I'm sorry, Dave. I'm afraid I can't do that")
 //   } else if (i % 3 === 0){
 //     console.log("Boop!")
@@ -19,12 +27,13 @@ function boopNumber(userInput) {
   //  User Logic
 
 
-  $(function() {
-    $("form").submit(function(event) {
-      event.preventDefault();
-      var userInput = $("#inputNumber").val();
-      var output = boopNumber(userInput);
-      $("#output").text(output);
-      console.log(output)
-    })
+$(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    console.log(userInput)
+    var userInput = $("#inputNumber").val();
+    var output = boopNumber(userInput);
+    $("#output").text(output);
+    console.log(output)
   })
+})
